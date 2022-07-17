ffa5a638c68b23531bb747c9d8eaa4f36e0328ea
Anna Babay <kot-vmeshke.4@gmail.com>
2022-07-17 20:49:59 +0200

    feat: add main-test.js and form-test.js

---
+++ scripts/form-test.js
@@ -0,0 +1,72 @@
+const set2 = document.querySelector('.reg__set2');
+const prodamus = set2.querySelector('#prodamus');
+const robokassa = set2.querySelector('#robokassa');
+
+const btnNext = document.querySelector('.reg__btn');
+const tab1 = document.querySelector('.tab-1');
+const tab2 = document.querySelector('.tab-2');
+const title = document.querySelector('.reg__title');
+const salary = document.querySelector('.reg__subtitle span');
+
+title.innerText = localStorage.getItem('title');
+salary.innerText = `от ${localStorage.getItem('salary')} руб`;
+
+const order = document.querySelector('.reg__order');
+const userName = document.querySelector('.reg__user-name');
+const userPhone = document.querySelector('.reg__user-phone');
+const userMail = document.querySelector('.reg__user-mail');
+
+const inputName = document.querySelector('#user-name');
+const inputPhone = document.querySelector('#user-phone');
+const inputMail = document.querySelector('#user-mail');
+
+const searchString = new URLSearchParams(window.location.search);
+console.log('searchString: ', searchString);
+const utmSource = searchString.get('utm_source') || '';
+const utmMedium = searchString.get('utm_medium') || '';
+const utmContent = searchString.get('utm_content') || '';
+const utmCampaign = searchString.get('utm_campaign') || '';
+const utmGroup = searchString.get('utm_group') || '';
+
+prodamus.addEventListener('change', () => {
+  set2.style.left = 'auto';
+  set2.style.right = '25px';
+})
+robokassa.addEventListener('change', () => {
+  set2.style.left = '';
+  set2.style.right = '';
+})
+btnNext.addEventListener('click', () => {
+  if (inputName.value && inputPhone.value && inputMail.value) {
+    tab1.style.display = 'none';
+    tab2.style.display = 'block';
+    console.log({
+      user_guid: localStorage.getItem('guid'),
+      name: inputName.value,
+      email: inputMail.value,
+      phone: inputPhone.value,
+      order_id: localStorage.getItem('order'),
+      back_link: `https://front.end/index.html?order_id=${localStorage.getItem('order')}`,
+      utm_source: utmSource,
+      utm_medium: utmMedium,
+      utm_content: utmContent,
+      utm_campaign: utmCampaign,
+      utm_group: utmGroup,
+    })
+    fetch('https://webhook.site/a3180ce4-c030-43cd-9e88-42a9d0d52726', {
+      method: 'post',
+    }).then((response) => {
+      if (response.ok) {
+        return response.text();
+      } else {
+        console.error('error');
+      }
+    }).then(data => console.log(data))
+    order.innerText = `Заказ №${localStorage.getItem('order')}`;
+    userName.innerText = inputName.value;
+    userPhone.innerText = inputPhone.value;
+    userMail.innerText = inputMail.value;
+  } else {
+    alert('Заполните все поля');
+  }
+})
--- scripts/main-test.js
+++ scripts/main-test.js
@@ -4,7 +4,7 @@ window.addEventListener('load', () => {
 
   if (localStorage.getItem('guid')) {
     let guid = localStorage.getItem('guid');
-    fetch(`https://marketplace-academica.ru/academica/get_ul?user_guid=${guid}`, {
+    fetch(`https://webhook.site/a3180ce4-c030-43cd-9e88-42a9d0d52726`, {
       method: 'POST',
     }).then((response) => {
       if (response.ok) {
@@ -16,32 +16,20 @@ window.addEventListener('load', () => {
       const [btn] = buttons.filter(item => item !== null);
       if(btn) {
         btn.addEventListener('click', () => {
-          localStorage.setItem('title', btn.dataset.title);
-          localStorage.setItem('salary', btn.dataset.salary);
-          fetch(`https://marketplace-academica.ru/choose_plan?user_guid=${guid}&plan_id=${btn.dataset.id}&cost=${btn.dataset.price}`, {
+          localStorage.setItem('title', 'Ассистент + аккаунт-менеджер Wildberries');
+          localStorage.setItem('salary', '45 000');
+          fetch(`https://webhook.site/a3180ce4-c030-43cd-9e88-42a9d0d52726`, {
             method: 'POST',
           }).then((response) => {
             return response.text();
           }).then(data => {
-            localStorage.setItem('order', data.data.order_id);
+            localStorage.setItem('order', '000002');
             window.location.href = './form.html';
           })
         })
       }
     });   
   } else {
-    // fetch('https://marketplace-academica.ru/get_ul', {
-    //   method: 'POST',      
-    // }).then((response) => {
-    //   if (response.ok) {
-    //     return response.text();
-    //   } else {
-    //     console.error('error');
-    //   }
-    // }).then(data => {
-    //   console.log(data);
-    //   localStorage.setItem('guid', data.data.user_guid);
-    // })
     fetch('https://webhook.site/a3180ce4-c030-43cd-9e88-42a9d0d52726', {
       method: 'POST',      
     }).then((response) => {
