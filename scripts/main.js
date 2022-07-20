c25343d29b218bc5b717dc1e915b74c6b84e8aa2
Anna Babay <kot-vmeshke.4@gmail.com>
2022-07-20 14:38:17 +0200

    feat: add switch from main to form

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
 window.addEventListener('load', () => {
 
   const buttons = [document.querySelector('.price__btn_research'), document.querySelector('.price__btn_assistant'), document.querySelector('.price__btn_project')];
-
+  const btnsMain = document.querySelectorAll('.rate__offer-btn');
+  
   if (localStorage.getItem('guid')) {
     let guid = localStorage.getItem('guid');
     fetch(`https://marketplace-academica.ru/academica/get_ul?user_guid=${guid}`, {
@@ -27,6 +28,24 @@ window.addEventListener('load', () => {
       const centerFooter = document.querySelector('.footer__center');
       centerFooter.innerText = `${localStorage.getItem('org_name')}${localStorage.getItem('org_data')}`;
       const [btn] = buttons.filter(item => item !== null);
+      btnsMain.forEach(btn => {
+        btn.addEventListener('click', () => {
+          localStorage.setItem('title', btn.dataset.title);
+          localStorage.setItem('salary', btn.dataset.salary);
+          localStorage.setItem('price', btn.dataset.price);
+          localStorage.setItem('part6', btn.dataset.part6);
+          localStorage.setItem('part10', btn.dataset.part10);
+          localStorage.setItem('part12', btn.dataset.part12);
+          fetch(`https://marketplace-academica.ru/academica/choose_plan?user_guid=${guid}&plan_id=${btn.dataset.id}&cost=${btn.dataset.price}`, {
+            method: 'POST',
+          }).then((response) => {
+            return response.json();
+          }).then(data => {
+            localStorage.setItem('order', data.data.order_id);
+            window.location.href = './pages/form.html';
+          })
+        })
+      })
       if(btn) {
         btn.addEventListener('click', () => {
           localStorage.setItem('title', btn.dataset.title);
@@ -44,8 +63,7 @@ window.addEventListener('load', () => {
             window.location.href = './form.html';
           })
         })
-      }
-  
+      }  
     });   
   } else {
     fetch('https://marketplace-academica.ru/academica/get_ul', {
@@ -72,6 +90,24 @@ window.addEventListener('load', () => {
       const centerFooter = document.querySelector('.footer__center');
       centerFooter.innerText = `${localStorage.getItem('org_name')}${localStorage.getItem('org_data')}`;
       const [btn] = buttons.filter(item => item !== null);
+      btnsMain.forEach(btn => {
+        btn.addEventListener('click', () => {
+          localStorage.setItem('title', btn.dataset.title);
+          localStorage.setItem('salary', btn.dataset.salary);
+          localStorage.setItem('price', btn.dataset.price);
+          localStorage.setItem('part6', btn.dataset.part6);
+          localStorage.setItem('part10', btn.dataset.part10);
+          localStorage.setItem('part12', btn.dataset.part12);
+          fetch(`https://marketplace-academica.ru/academica/choose_plan?user_guid=${guid}&plan_id=${btn.dataset.id}&cost=${btn.dataset.price}`, {
+            method: 'POST',
+          }).then((response) => {
+            return response.json();
+          }).then(data => {
+            localStorage.setItem('order', data.data.order_id);
+            window.location.href = './pages/form.html';
+          })
+        })
+      })
       if(btn) {
         btn.addEventListener('click', () => {
           localStorage.setItem('title', btn.dataset.title);
