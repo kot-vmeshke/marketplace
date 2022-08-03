e5242b7b19cc2481502dba60914c6693ffd6c7d9
Anna Babay <kot-vmeshke.4@gmail.com>
2022-08-03 14:27:59 +0200

    feat: add loan

--- scripts/form.js
+++ scripts/form.js
@@ -8,13 +8,6 @@ const price = document.querySelector(".reg__price"); // сумма оплаты,
 const spinner = document.querySelector('#preloader'); // прелоадер
 const tab1 = document.querySelector(".tab-1"); // первая часть формы
 const tab2 = document.querySelector(".tab-2"); // вторая часть формы
-let fullCost; // полная стоимость тарифа
-let costPart6; // цена рассрочки на 6 месяцев
-let costPart10; // цена рассрочки на 10 месяцев
-let costPart12; // цена рассрочки на 12 месяцев
-let cost; // сумма, которую видит юзер в рассрочке
-let partCost; // полная сумма рассрочки, отправляем в заявке
-let planID; // ID тарифа
 const order = document.querySelector(".reg__order"); // номер заказа
 const userName = document.querySelector(".reg__user-name"); // имя пользователя
 const userPhone = document.querySelector(".reg__user-phone"); // телефон пользователя
@@ -36,6 +29,24 @@ const part6 = months.querySelector("#month-6");
 const part10 = months.querySelector("#month-10");
 const part12 = months.querySelector("#month-12");
 
+/* стоимость тарифа */
+let fullCost; // полная стоимость тарифа
+let costPart6; // цена рассрочки на 6 месяцев
+let costPart6Prepay; // цена рассрочки на 6 месяцев с предоплатой
+let costPart6Promo; // цена рассрочки на 6 месяцев с промокодом
+let costPart6PrepayPromo; // цена рассрочки на 6 месяцев с предоплатой и промокодом
+let costPart10; // цена рассрочки на 10 месяцев
+let costPart10Prepay; // цена рассрочки на 10 месяцев с предоплатой
+let costPart10Promo; // цена рассрочки на 10 месяцев с промокодом
+let costPart10PrepayPromo; // цена рассрочки на 10 месяцев с предоплатой и промокодом
+let costPart12; // цена рассрочки на 12 месяцев
+let costPart12Prepay; // цена рассрочки на 12 месяцев с предоплатой
+let costPart12Promo; // цена рассрочки на 12 месяцев с промокодом
+let costPart12PrepayPromo; // цена рассрочки на 12 месяцев с предоплатой и промокодом
+let cost; // сумма, которую видит юзер в рассрочке
+let partCost; // полная сумма рассрочки, отправляем в заявке
+let planID; // ID тарифа
+
 /* инпуты */
 const inputName = document.querySelector("#user-name");
 const inputPhone = document.querySelector("#user-phone");
@@ -50,21 +61,8 @@ const inputFathername = document.querySelector("#user-fathername");
 /* промокод */
 const inputPromo = document.querySelector(".promo");
 const btnPromo = document.querySelector(".promo-btn");
-
-btnPromo.addEventListener("click", () => {
-  if (inputPromo.value.toUpperCase() === "chek3000".toUpperCase()) {
-    btnPromo.style.backgroundColor = "#00904B";
-    inputPromo.style.color = "#00904B";
-    fullCost = fullCost - 3000;
-    price.innerText = `${fullCost}₽`;
-  } else if (inputPromo.value.toUpperCase() !== "chek3000".toUpperCase()) {
-    btnPromo.style.backgroundColor = "#FF0000";
-    inputPromo.value = "";
-    inputPromo.placeholder = "Неверный промокод";
-  }
-});
-
-
+const PROMO_CODE = "chek3000";
+let promo = false;
 
 window.addEventListener('load', () => {
   /* определяем, какой тариф выбрал пользователь */
@@ -94,8 +92,40 @@ window.addEventListener('load', () => {
   let resUlID;
   let resPlanID;
 
+  /* кнопка промокода*/
+  btnPromo.addEventListener("click", () => {
+    if (inputPromo.value.toUpperCase() === PROMO_CODE.toUpperCase()) {
+      btnPromo.style.backgroundColor = "#00904B";
+      inputPromo.style.color = "#00904B";
+      fullCost = fullCost - 3000;
+      promo = true;
+      switch (currentTarif) {
+        case '3544961':
+          costPart6 = 22000; 
+          costPart10 = 23000; 
+          costPart12 = 24000;
+          break;
+        case '3544966':
+          costPart6 = 38800; 
+          costPart10 = 40500; 
+          costPart12 = 41200;
+          break;
+        case '3544972':
+          costPart6 = 54900; 
+          costPart10 = 57000; 
+          costPart12 = 59000;
+          break;
+      }
+      price.innerText = `${fullCost}₽`;
+    } else if (inputPromo.value.toUpperCase() !== PROMO_CODE.toUpperCase()) {
+      btnPromo.style.backgroundColor = "#FF0000";
+      inputPromo.value = "";
+      inputPromo.placeholder = "Неверный промокод";
+    }
+  });
+
   /* переключение способов оплаты */
-  function choosePayment() {
+  function choosePayment(resCost, costPart6, costPart10, costPart12) {
     set1.addEventListener("click", (event) => {
       if (event.target.classList.contains("js-radio-set1") && event.target.classList.contains("ip-ooo")) { // ИП
         set2.style.display = "none";
@@ -104,7 +134,7 @@ window.addEventListener('load', () => {
         regPart.style.display = "";
         robokassa.checked = false;
         regBtn.querySelector(".reg__btn-text").innerText = "Перейти к оплате";
-        price.innerText = `${fullCost}₽`;
+        price.innerText = `${resCost}₽`;
       } else if (event.target.classList.contains("js-radio-set1") && event.target.classList.contains("part")) { // рассрочка
         set2.style.display = "none";
         regPart.style.display = "block";
@@ -138,7 +168,7 @@ window.addEventListener('load', () => {
         regPart.style.display = "";
         regBtn.querySelector(".reg__btn-text").innerText = "Перейти к оплате";
         robokassa.checked = true;
-        price.innerText = `${fullCost}₽`;
+        price.innerText = `${resCost}₽`;
       } else if ( event.target.classList.contains("js-radio-set1") && event.target.classList.contains("prepay") ) { // предоплата 2000
         set2.style.display = "";
         price.style.marginTop = "";
@@ -171,13 +201,11 @@ window.addEventListener('load', () => {
           .then((data) => {
             console.log(data);
             if (data.error == 1) {
-              alert(`Error: ${data.message}\nЧто-то пошло не так, воспользуйтесь виджетом для обратной связи или напишите письмо на info@marketplace-academica.ru`);
+              alert(`Error: ${data.message}\nВоспользуйтесь виджетом для обратной связи или напишите письмо на info@marketplace-academica.ru`);
               throw new Error(`Error: ${data.message}`);
             } else {
-              setTimeout(() => {
-                window.location.href = "./order.html";
-              }, 0);
-              window.open(`${data.data.payment_link}`, "_blank").focus();
+              window.location.href = "./order.html";
+              //window.open(`${data.data.payment_link}`, "_blank").focus();
             }
           });
       } else if (part.checked) { // рассрочка
@@ -219,7 +247,7 @@ window.addEventListener('load', () => {
           .then((data) => {
             console.log(data);
             if (data.error == 1) {
-              alert(`Error: ${data.message}\nЧто-то пошло не так, воспользуйтесь виджетом для обратной связи или напишите письмо на info@marketplace-academica.ru`);
+              alert(`Error: ${data.message}\nВоспользуйтесь виджетом для обратной связи или напишите письмо на info@marketplace-academica.ru`);
               throw new Error(`Error: ${data.message}`);
             } else {
               window.location.href = data.data.payment_link;
@@ -242,7 +270,7 @@ window.addEventListener('load', () => {
           .then((data) => {
             console.log(data);
             if (data.error == 1) {
-              alert(`Error: ${data.message}\nЧто-то пошло не так, воспользуйтесь виджетом для обратной связи или напишите письмо на info@marketplace-academica.ru`);
+              alert(`Error: ${data.message}\nВоспользуйтесь виджетом для обратной связи или напишите письмо на info@marketplace-academica.ru`);
               throw new Error(`Error: ${data.message}`);
             } else {
               window.location.href = data.data.payment_link;
@@ -265,7 +293,7 @@ window.addEventListener('load', () => {
           .then((data) => {
             console.log(data);
             if (data.error == 1) {
-              alert(`Error: ${data.message}\nЧто-то пошло не так, воспользуйтесь виджетом для обратной связи или напишите письмо на info@marketplace-academica.ru`);
+              alert(`Error: ${data.message}\nВоспользуйтесь виджетом для обратной связи или напишите письмо на info@marketplace-academica.ru`);
               throw new Error(`Error: ${data.message}`);
             } else {
               window.location.href = data.data.payment_link;
@@ -288,7 +316,7 @@ window.addEventListener('load', () => {
           .then((data) => {
             console.log(data);
             if (data.error == 1) {
-              alert(`Error: ${data.message}\nЧто-то пошло не так, воспользуйтесь виджетом для обратной связи или напишите письмо на info@marketplace-academica.ru`);
+              alert(`Error: ${data.message}\nВоспользуйтесь виджетом для обратной связи или напишите письмо на info@marketplace-academica.ru`);
               throw new Error(`Error: ${data.message}`);
             } else {
               window.location.href = data.data.payment_link;
@@ -342,7 +370,7 @@ window.addEventListener('load', () => {
           } else { // если ответ пришел
             let data = JSON.parse(xhr.response);
             if (data.error == 1) { // если пришел с ошибкой
-              alert(`Error: ${data.message}\nЧто-то пошло не так, воспользуйтесь виджетом для обратной связи или напишите письмо на info@marketplace-academica.ru`);
+              alert(`Error: ${data.message}`);
               spinner.classList.add('hidden');
               throw new Error(`Error: ${data.message}`);
             } else { // если все норм
@@ -368,7 +396,7 @@ window.addEventListener('load', () => {
               price.innerText = `${resCost}₽`;
               ipTextUserId.innerText = `id-${resOrder}`;    
     
-              choosePayment();
+              choosePayment(resCost, costPart6, costPart10, costPart12);
               getPayLink(resOrder, resEmail);
     
             }          
@@ -397,51 +425,118 @@ window.addEventListener('load', () => {
     .then(data => {
       console.log(data);
       if (data.error == 1) {
-        alert(`Error: ${data.message}\nЧто-то пошло не так, воспользуйтесь виджетом для обратной связи или напишите письмо на info@marketplace-academica.ru`);
+        alert(`Error: ${data.message}\nВоспользуйтесь виджетом для обратной связи или напишите письмо на info@marketplace-academica.ru`);
         spinner.classList.add('hidden');
         throw new Error(`Error: ${data.message}`);
       } else {
         spinner.classList.add('hidden');
-        /* сохраняем данные из ответа в переменные */
-        let resName = data.data.user.name;
-        let resEmail = data.data.user.email;
-        let resPhone = data.data.user.phone;
-        let resOrder = data.data.order.order_id;
-        let resCost = data.data.order.cost;
-        let resPlanID = data.data.order.plan_id;
-        let resOferta = data.data.org.oferta;
-        let resPolicy = data.data.org.conf_policy;
-        let resOrgName = data.data.org.org_name;
-        let resOrgData = data.data.org.org_data;
 
+        /* сохраняем данные из ответа в переменные */
+        resName = data.data.user.name;
+        resEmail = data.data.user.email;
+        resPhone = data.data.user.phone;
+        resOrder = data.data.order.order_id;
+        resCost = data.data.order.cost;
+        resPlanID = data.data.order.plan_id;
+        resOferta = data.data.org.oferta;
+        resPolicy = data.data.org.conf_policy;
+        resOrgName = data.data.org.org_name;
+        resOrgData = data.data.org.org_data;
+
+        /* сравниваем полную цену тарифа и цену из ответа */
+        let diff;
 
         /* в зависимости от тарифа присваиваем переменным значения */
         switch (resPlanID) {
           case 3544961:
             pageTitle.innerText = "Ассистент + аккаунт-менеджер Wildberries";
             salary.innerText = 'от 45 000 руб.';
-            fullCost = resCost;
-            costPart6 = 20000; 
-            costPart10 = 21000; 
-            costPart12 = 22000; 
+            fullCost = 23000;
+            diff = fullCost - resCost;
+            /* определяем цену в рассрочку */
+            switch(diff) {
+              case 0:
+                costPart6 = 25500; 
+                costPart10 = 26500; 
+                costPart12 = 27000;
+                break;
+              case 2000:
+                costPart6 = 23000; 
+                costPart10 = 24000; 
+                costPart12 = 25000;
+                break;
+              case 3000:
+                costPart6 = 22000; 
+                costPart10 = 23000; 
+                costPart12 = 24000;
+                break;
+              case 5000:
+                costPart6 = 19800; 
+                costPart10 = 21000; 
+                costPart12 = 21300;
+                break;
+            }
             break;
           case 3544966:
             pageTitle.innerText = "Research manager + Специалист отдела закупок";
             salary.innerText = 'от 75 000 руб.';
-            fullCost = resCost;
-            costPart6 = 36600; 
-            costPart10 = 38200; 
-            costPart12 = 39200;
+            fullCost = 38000;
+            diff = fullCost - resCost;
+            /* определяем цену в рассрочку */
+            switch(diff) {
+              case 0:
+                costPart6 = 42000; 
+                costPart10 = 43800; 
+                costPart12 = 44500;
+                break;
+              case 2000:
+                costPart6 = 39800; 
+                costPart10 = 41500; 
+                costPart12 = 42500;
+                break;
+              case 3000:
+                costPart6 = 38800; 
+                costPart10 = 40500; 
+                costPart12 = 41200;
+                break;
+              case 5000:
+                costPart6 = 36500; 
+                costPart10 = 38200; 
+                costPart12 = 38800;
+                break;
+            }
             break;
           case 3544972:
             pageTitle.innerText = "Project manager / Управляющий проектом";
             salary.innerText = 'от 90 000 руб.';
-            fullCost = resCost;
-            costPart6 = 53250; 
-            costPart10 = 55800; 
-            costPart12 = 56900;
+            fullCost = 53000;
+            diff = fullCost - resCost;
+            /* определяем цену в рассрочку */
+            switch(diff) {
+              case 0:
+                costPart6 = 58700; 
+                costPart10 = 61000; 
+                costPart12 = 62500;
+                break;
+              case 2000:
+                costPart6 = 56300; 
+                costPart10 = 58900; 
+                costPart12 = 60000;
+                break;
+              case 3000:
+                costPart6 = 54900; 
+                costPart10 = 57000; 
+                costPart12 = 59000;
+                break;
+              case 5000:
+                costPart6 = 52500; 
+                costPart10 = 54900; 
+                costPart12 = 57000;
+                break;
+            }
             break;
         }
+        
         if (resName.length === 0 || resPhone.length === 0 || resEmail.length === 0) {
           /* если нет имени, телефона или почты, показываем первую часть формы */
           tab1.style.display = "block";
@@ -454,7 +549,7 @@ window.addEventListener('load', () => {
           userName.innerText = resName;
           userPhone.innerText = resPhone;
           userMail.innerText = resEmail;
-          price.innerText = `${fullCost}₽`;
+          price.innerText = `${resCost}₽`;
           ipTextUserId.innerText = `id-${resOrder}`;
   
           /* выводим данные в футер и показываем его */
@@ -463,13 +558,61 @@ window.addEventListener('load', () => {
           document.querySelector(".footer__center").innerText = `${resOrgName}${resOrgData}`;
           footer.style.display = 'block';
   
-          choosePayment();
+          choosePayment(resCost, costPart6, costPart10, costPart12);
           getPayLink(resOrder, resEmail);
 
         }
       }
     })
   } else {
+    /* в зависимости от выбранного тарифа присваиваем переменным значения */
+    switch (currentTarif) {
+      case '3544961':
+        pageTitle.innerText = "Ассистент + аккаунт-менеджер Wildberries";
+        salary.innerText = 'от 45 000 руб.';
+        fullCost = 23000;
+        if (promo) {
+          costPart6 = 22000; 
+          costPart10 = 23000; 
+          costPart12 = 24000;
+        } else {
+          costPart6 = 25500; 
+          costPart10 = 26500; 
+          costPart12 = 27000; 
+        }
+        planID = '3544961';
+        break;
+      case '3544966':
+        pageTitle.innerText = "Research manager + Специалист отдела закупок";
+        salary.innerText = 'от 75 000 руб.';
+        fullCost = 38000;
+        if (promo) {
+          costPart6 = 38800; 
+          costPart10 = 40500; 
+          costPart12 = 41200;
+        } else {
+          costPart6 = 42000; 
+          costPart10 = 43800; 
+          costPart12 = 44500; 
+        }
+        planID = '3544966';
+        break;
+      case '3544972':
+        pageTitle.innerText = "Project manager / Управляющий проектом";
+        salary.innerText = 'от 90 000 руб.';
+        fullCost = 53000;
+        if (promo) {
+          costPart6 = 54900; 
+          costPart10 = 57000; 
+          costPart12 = 59000;
+        } else {
+          costPart6 = 58700; 
+          costPart10 = 61000; 
+          costPart12 = 62500; 
+        }
+        planID = '3544972';
+        break;
+    }
     /* отправляем запрос для получения данных юрлица*/
     fetch('https://marketplace-academica.ru/academica/get_ul', {method: 'POST'})
     .then((response) => {
@@ -483,39 +626,10 @@ window.addEventListener('load', () => {
     .then(data => {
       console.log(data);
       if (data.error == 1) {
-        alert(`Error: ${data.message}\nЧто-то пошло не так, воспользуйтесь виджетом для обратной связи или напишите письмо на info@marketplace-academica.ru`);
+        alert(`Error: ${data.message}\nВоспользуйтесь виджетом для обратной связи или напишите письмо на info@marketplace-academica.ru`);
         throw new Error(`Error: ${data.message}`);
       } else {
-        /* в зависимости от выбранного тарифа присваиваем переменным значения */
-        switch (currentTarif) {
-          case '3544961':
-            pageTitle.innerText = "Ассистент + аккаунт-менеджер Wildberries";
-            salary.innerText = 'от 45 000 руб.';
-            fullCost = 23000;
-            costPart6 = 20000; 
-            costPart10 = 21000; 
-            costPart12 = 22000; 
-            planID = '3544961';
-            break;
-          case '3544966':
-            pageTitle.innerText = "Research manager + Специалист отдела закупок";
-            salary.innerText = 'от 75 000 руб.';
-            fullCost = 38000;
-            costPart6 = 36600; 
-            costPart10 = 38200; 
-            costPart12 = 39200;
-            planID = '3544966';
-            break;
-          case '3544972':
-            pageTitle.innerText = "Project manager / Управляющий проектом";
-            salary.innerText = 'от 90 000 руб.';
-            fullCost = 53000;
-            costPart6 = 53250; 
-            costPart10 = 55800; 
-            costPart12 = 56900;
-            planID = '3544972';
-            break;
-        }
+
         /* выводим на страницу сумму оплаты */
         price.innerText = `${fullCost}₽`;
 
@@ -532,7 +646,7 @@ window.addEventListener('load', () => {
         document.querySelector(".footer__center").innerText = `${resOrgName}${resOrgData}`;
         footer.style.display = 'block';
 
-        /* dspsdftv функцию для отправки данных */
+        /* вызываем функцию для отправки данных */
         sendData();
 
       }})
